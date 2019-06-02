84dddb76295b453298268762dbeb15f87f4e7e80
SNAP Hydroponics <snaphydroponics@gmail.com>
2019-06-02 21:01:34 +0800

    WIP.

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,55 +1,41 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form';
-import FeedbackButton from './FeedbackButton';
-import axios from 'axios';
-import {Formik} from 'formik';
+import FeedbackForm from './FeedbackForm';
 
 class Feedback extends React.Component {
   constructor(props) {
     super(props)
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
     this.state = {
       loading: false,
       success: null
     };
   }
 
-  handleChange = e => this.setState({[e.target.name]: e.target.value}); 
-
-  handleSubmit(values) {
-
-    const config = {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    };
-
-    this.setState({
-      loading: true
-    });
-
-    axios.post('/', values, config).then((result) => {
-      console.log(result);
-      this.setState({
-        success: true
-      });
-    }).catch((err) => {
-      console.log(err);
-      this.setState({
-        success: false
-      });
-    }).finally(() => {
-      this.setState({
-        loading: false
-      });
-    });
-  }
 
   render() {
     const lang = this.props.match.url.match(/^\/en/) ? 'en' : 'fil';
-    const { loading } = this.state;
-    return(
+    const { loading, success } = this.state;
+
+    let display = (<FeedbackForm loading={loading} lang={lang} />);
+
+    if (success === true) {
+      display =  (
+        <div class="alert alert-success my-3" role="alert">
+          <h4 class="alert-heading">Feedback Sent!</h4>
+          <p>Thank you for helping us improve this app.</p>
+        </div>
+      );
+    }
+
+    if (success === false) {
+      display = (
+        <div class="alert alert-warning my-3" role="alert">
+          <h4 class="alert-heading">Failed to send feed back</h4>
+          <p>Please try again later.</p>
+        </div>
+      );
+    }
+
+    return (
       <div className="container">
         <header className="my-5">
           <h1 id="content">{
@@ -64,54 +50,10 @@ class Feedback extends React.Component {
         }</p>
 
         </header>
-        <Formik
-          initialValues={{ type: '', details: ''}}
-          validate={(values) => {
-            let errors = {};
-            if (!values.type) {
-              errors.type = true;
-            }
-
-            if (!values.details) {
-              errors.details = true;
-            }
-            return errors;
-          }}
-          onSubmit={this.handleSubmit}>
-            {({
-              handleSubmit,
-              handleChange,
-              handleBlur,
-              values,
-              touched,
-              isValid,
-              errors
-            }) => (
-          <Form noValidate disabled={loading} className="my-3" onSubmit={handleSubmit}>
-            <Form.Group controlId="feedback-type">
-              <Form.Label size="lg" as="legend" column>{
-                lang === 'en' ?  "I’m telling you about" : "Ang aking ipinaaalam ay tungkol sa."}
-              </Form.Label>
-              <Form.Control required onChange={handleChange} value={values.type} name="type" size="lg" as="select" isInvalid={touched.type && errors.type}>
-                <option value="">{ lang === 'en' ? 'Choose…' : 'Pumili…' }</option>
-                <option value="bug-typo">{ lang === 'en' ? 'A bug or typographical error.' : 'Bug o typographical na pagkakamali'}</option>
-                <option value="enhancement">{ lang === 'en' ? 'An enhancement or feature request' : 'Pagpapahusay o paghiling ng feature'}</option>
-                <option value="kudos">{ lang === 'en' ? 'Kudos' : 'Pagpuri' }</option>
-              </Form.Control>
-              <Form.Control.Feedback type="invalid">{ lang === 'en' ? 'Please choose.' : 'Mangyaring pumili.' }</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="feedback-details">
-              <Form.Label size="lg" as="legend" column>{ lang === 'en' ? 'Details' : 'Mga detalye' }</Form.Label>
-              <Form.Control required onChange={handleChange} value={values.details} name="details" size="lg" as="textarea" rows="5" placeholder={ lang === 'en' ? "Details…" : "Mga detalye…" } isInvalid={touched.type && errors.details} ></Form.Control>
-              <Form.Control.Feedback type="invalid">{ lang === 'en' ? 'Please fill this out.' : 'Mangyaring punan ito.' }</Form.Control.Feedback>
-            </Form.Group>
-            <FeedbackButton loading={loading} lang={lang} />
-            <input type="hidden" name="form-name" value="feedback" />
-          </Form>
-          )
-        }</Formik>
+        {display}
       </div>
     );
+
   }
 }
 
---
+++ src/FeedbackForm.js
@@ -0,0 +1,94 @@
+import {Formik} from 'formik';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import FeedbackButton from './FeedbackButton';
+import axios from 'axios';
+
+export default class FeedbackForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(values) {
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+
+    this.setState({
+      loading: true
+    });
+
+    axios.post('/', values, config).then((result) => {
+      console.log(result);
+      this.setState({
+        success: true
+      });
+    }).catch((err) => {
+      console.log(err);
+      this.setState({
+        success: false
+      });
+    }).finally(() => {
+      this.setState({
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    const {loading, lang} = this.props;
+    return (
+        <Formik
+          initialValues={{ type: '', details: ''}}
+          validate={(values) => {
+            let errors = {};
+            if (!values.type) {
+              errors.type = true;
+            }
+
+            if (!values.details) {
+              errors.details = true;
+            }
+            return errors;
+          }}
+          onSubmit={this.handleSubmit}>
+            {({
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              values,
+              touched,
+              isValid,
+              errors
+            }) => (
+          <Form noValidate disabled={loading} className="my-3" onSubmit={handleSubmit}>
+            <Form.Group controlId="feedback-type">
+              <Form.Label size="lg" as="legend" column>{
+                lang === 'en' ?  "I’m telling you about" : "Ang aking ipinaaalam ay tungkol sa."}
+              </Form.Label>
+              <Form.Control disabled={loading} required onChange={handleChange} value={values.type} name="type" size="lg" as="select" isInvalid={touched.type && errors.type}>
+                <option value="">{ lang === 'en' ? 'Choose…' : 'Pumili…' }</option>
+                <option value="bug-typo">{ lang === 'en' ? 'A bug or typographical error.' : 'Bug o typographical na pagkakamali'}</option>
+                <option value="enhancement">{ lang === 'en' ? 'An enhancement or feature request' : 'Pagpapahusay o paghiling ng feature'}</option>
+                <option value="kudos">{ lang === 'en' ? 'Kudos' : 'Pagpuri' }</option>
+              </Form.Control>
+              <Form.Control.Feedback type="invalid">{ lang === 'en' ? 'Please choose.' : 'Mangyaring pumili.' }</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="feedback-details">
+              <Form.Label size="lg" as="legend" column>{ lang === 'en' ? 'Details' : 'Mga detalye' }</Form.Label>
+              <Form.Control disabled={loading} required onChange={handleChange} value={values.details} name="details" size="lg" as="textarea" rows="5" placeholder={ lang === 'en' ? "Details…" : "Mga detalye…" } isInvalid={touched.type && errors.details} ></Form.Control>
+              <Form.Control.Feedback type="invalid">{ lang === 'en' ? 'Please fill this out.' : 'Mangyaring punan ito.' }</Form.Control.Feedback>
+            </Form.Group>
+            <FeedbackButton loading={loading} lang={lang} />
+            <input type="hidden" name="form-name" value="feedback" />
+          </Form>
+          )
+        }</Formik>
+    );
+  }
+}
+
