efb4b5291b6cea135eae5ee3f474b18f269644a3
SNAP Hydroponics <snaphydroponics@gmail.com>
2019-06-08 17:30:44 +0800

    Fix language selector.

--- src/EnSelector.js
+++ src/EnSelector.js
@@ -6,9 +6,8 @@ class EnSelector extends React.Component {
 
   render() {
     const pathname = this.props.location.pathname;
-    //const label = pathname.match(/^\/en/) ? 'English' : 'Ingles';
+    const url = pathname.match(/^\/en/) ? pathname : ('/en' + pathname);
     const label = 'English';
-    const url = (label === 'English') ? pathname : ('/en' + pathname);
 
     return (
       <LinkContainer exact to={url}><Nav.Link>{label}</Nav.Link></LinkContainer>
